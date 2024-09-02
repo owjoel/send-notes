220d7221727ec53db5e36a7941be32d3c764882b
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-09-02 22:04:32 +0800

    feat: Set up Notes Service

---
+++ apps/notes/src/test/java/cs302/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package cs302.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
