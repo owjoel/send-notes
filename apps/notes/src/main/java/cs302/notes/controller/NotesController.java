f45d1c3caf0985bc2ca06f0c9334e3e4bcb9f22a
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-09-03 13:18:18 +0800

    feat: Create endpoint for getNotesById and getNotes

--- apps/notes/src/main/java/cs302/notes/controller/NotesController.java
+++ apps/notes/src/main/java/cs302/notes/controller/NotesController.java
@@ -6,6 +6,7 @@ import cs302.notes.data.response.Response;
 import cs302.notes.service.services.NotesService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,9 +28,22 @@ public class NotesController {
         return new ResponseEntity(DefaultResponse.builder().message("Hello World!").build(), HttpStatus.OK);
     }
 
+    @GetMapping("/notes")
+    public ResponseEntity<Response> getNotes(@RequestParam(defaultValue = "0") int page,
+                                             @RequestParam(defaultValue = "10") int limit) {
+        Response response = notesService.getNotes(page, limit);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
     @PostMapping("/notes")
     public ResponseEntity<Response> createNotes(@Valid @RequestBody NotesRequest request) {
         Response response = notesService.createNotes(request);
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/notes/{notesId}")
+    public ResponseEntity<Response> getNotesById(@PathVariable("notesId") String notesId) {
+        Response response = notesService.getNotesById(notesId);
         return new ResponseEntity(response, HttpStatus.OK);
     }
 }
