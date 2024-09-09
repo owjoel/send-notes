d306ea0c2be9806927fb727f9c8bbbe209d481a6
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-09-09 18:36:01 +0800

    feat: Implemented search by fk_account_owner with pagination on repository

--- apps/notes/src/main/java/cs302/notes/repository/NotesRepository.java
+++ apps/notes/src/main/java/cs302/notes/repository/NotesRepository.java
@@ -1,10 +1,13 @@
 package cs302.notes.repository;
 
 import cs302.notes.models.Notes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
 public interface NotesRepository extends MongoRepository<Notes, String> {
     Optional<Notes> findBy_id(String _id);
+    Page findByFkAccountOwner(String fkAccountOwner, Pageable pageable);
 }
