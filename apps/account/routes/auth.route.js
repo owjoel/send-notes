5f3656e3bdcbb0d7f6bf2dc9b091c4c4303fb51c
EddyEhhh <ehhitseddy@gmail.com>
2024-09-26 04:07:29 +0800

    [account] add: logout and check session

--- apps/account/routes/auth.route.js
+++ apps/account/routes/auth.route.js
@@ -1,11 +1,12 @@
 var express = require('express');
-const {callback, refreshToken, authTest} = require("../controllers/auth");
+const {callback, refreshToken, authTest, logout, isAuthenticated} = require("../controllers/auth");
 
 var router = express.Router();
 
 router.get('/callback', callback);
 router.get('/refreshToken', refreshToken);
 router.get('/test', authTest);
-
+router.get('/logout', logout);
+router.get('/', isAuthenticated)
 
 module.exports = router;
