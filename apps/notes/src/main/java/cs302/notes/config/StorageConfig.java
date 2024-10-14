958c37fb8a437c21c86744c6d48cef869619d007
owjoel <owjoel@gmail.com>
2024-10-15 01:55:24 +0800

    refactor: aws environment config

--- apps/notes/src/main/java/cs302/notes/config/StorageConfig.java
+++ apps/notes/src/main/java/cs302/notes/config/StorageConfig.java
@@ -3,28 +3,35 @@ package cs302.notes.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class StorageConfig {
 
-    @Value("${aws.accessKeyId}")
-    private String accessKey;
-    @Value("${aws.secretKey}")
-    private String secretKey;
     @Value("${aws.region}")
     private String region;
 
     @Bean
-    public AmazonS3 generateS3Client() {
+    @Profile("dev")
+    public AmazonS3 generateS3Client(@Value("${aws.accessKeyId}") String accessKey, @Value("${aws.secretKey}") String secretKey) {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region).build();
     }
 
+    @Bean
+    @Profile({"prod", "default"})
+    public AmazonS3 generateS3ClientProd() {
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(region)
+                .build();
+    }
 }
