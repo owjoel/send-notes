9d22533861594587f846ddb18dc23419e4220ce4
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-10-17 01:54:26 +0800

    feat: Implemented fix on listening on RabbitMQ

--- apps/notes/src/main/java/cs302/notes/consumer/MessageReceiver.java
+++ apps/notes/src/main/java/cs302/notes/consumer/MessageReceiver.java
@@ -1,9 +1,7 @@
 package cs302.notes.consumer;
 
-import cs302.notes.models.ListingStatus;
-import cs302.notes.models.OrderCreated;
+import cs302.notes.models.*;
 import cs302.notes.exceptions.NotesNotFoundException;
-import cs302.notes.models.OrderSuccess;
 import cs302.notes.producer.MessageSender;
 import cs302.notes.repository.NotesRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -27,6 +25,8 @@ public class MessageReceiver {
             messageSender.publishNotesFound(request);
         } catch (NotesNotFoundException e) {
             messageSender.publishNotesMissing(request);
+        } catch (Exception e) {
+            System.out.println("Error receiving verification");
         }
     }
 
@@ -34,19 +34,32 @@ public class MessageReceiver {
     public void receiveMessage(final OrderSuccess request) {
         try {
             System.out.println("Forwarded signed url for notification");
+            // Append stuff for notes and forward to eddy
+            Notes notes = repository.findBy_id(request.getNotesId())
+                    .orElseThrow(() -> new NotesNotFoundException(request.getNotesId()));
+            OrdersNotesSuccess ordersNotesSuccess = new OrdersNotesSuccess(request, notes);
+            messageSender.publishEmailClients(ordersNotesSuccess);
+
+        } catch (NotesNotFoundException e) {
+            System.out.println("Notes no longer found");
         } catch (Exception e) {
             System.out.println("Failed to send out notification");
         }
     }
 
-    @RabbitListener(queues = "${rabbitmq.orders.verified.queue}")
+    @RabbitListener(queues = "${rabbitmq.listings.verified.queue}")
     public void receiveMessage(final ListingStatus request) {
         try {
             System.out.println("Notes verified");
+            Notes notes = repository.findBy_id(request.get_id())
+                    .orElseThrow(() -> new NotesNotFoundException(request.get_id()));
+            notes.setStatus(request.getStatus());
+            repository.save(notes);
+            messageSender.publishListingCompleted(request);
+        } catch (NotesNotFoundException e) {
+            System.out.println("Notes no longer found");
         } catch (Exception e) {
             System.out.println("Error receiving verification");
         }
     }
-
-
 }
