0c9d79f789acf7ae5b4894779fc6c0ab458f28b5
owjoel <owjoel@gmail.com>
2024-10-17 21:09:27 +0800

    feat(request): amqp config

---
+++ apps/request/events/consumer.js
@@ -0,0 +1,33 @@
+const { notifyRequest } = require("../services/requestItemService");
+
+const notesFoundQ = "notes-found";
+const paymentCompletedQ = "payment-completed";
+
+async function connnectAMQP() {
+  try {
+    const conn = await amqp.connect({
+      protocol: process.env.RABBITMQ_PROTOCOL,
+      username: process.env.RABBITMQ_USERNAME,
+      password: process.env.RABBITMQ_PASSWORD,
+      hostname: process.env.RABBITMQ_HOST,
+      port: process.env.RABBITMQ_PORT,
+    });
+    console.log(`CONSUMER Connected: ${process.env.RABBITMQ_HOST}`);
+    ch = await conn.createChannel();
+    ch.assertExchange("orders", "topic");
+
+    await ch.assertQueue(notesFoundQ);
+    ch.bindQueue(notesFoundQ, "orders", "orders.notes.#");
+    await ch.consume(notesFoundQ, handleListingEvent);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// EDDY HELP THANKS HAHA
+const handleListingEvent = async (message) => {
+  const listing = JSON.parse(message.content.toString());
+  const res = await notifyRequest(listing);
+};
+
+module.exports = { connnectAMQP };
--- apps/request/services/requestItemService.js
+++ apps/request/services/requestItemService.js
@@ -1,116 +1,102 @@
-const RequestItem = require('../models/RequestItem');
-const {publishRequestNotify} = require("../adapters/events/rabbitmq/producer");
+const RequestItem = require("../models/RequestItem");
+const {
+  publishRequestNotify,
+} = require("../adapters/events/rabbitmq/producer");
 
 // Create new order
 
 class RequestItemService {
+  static async createRequest(user, requestData) {
+    const regex = /^[a-zA-Z0-9 _-]+$/;
 
-    static async createRequest(user, requestData) {
-
-        const regex = /^[a-zA-Z0-9 _-]+$/;
-
-        // Only allow these characters ( alpha numbers _ - space )
-        if (!regex.test(requestData.tag)) {
-            throw new Error('validation error request tag');
-        }
-
-        try {
-
-            const request = new RequestItem();
-            request.userId = user;
-            request.tag = requestData.tag;
-
-            await request.save();
-            return request
-        } catch (error) {
-            console.log(error);
-            throw new Error('internal server error');
-        }
+    // Only allow these characters ( alpha numbers _ - space )
+    if (!regex.test(requestData.tag)) {
+      throw new Error("validation error request tag");
     }
 
-    static async findById(requestId) {
+    try {
+      const request = new RequestItem();
+      request.userId = user;
+      request.tag = requestData.tag;
 
-
-
-        try {
-            // const exists = await RequestItem.exists({ _id: requestId })
-            return await RequestItem.findById(requestId);
-        } catch (error) {
-            throw new Error('internal server error');
-        }
+      await request.save();
+      return request;
+    } catch (error) {
+      console.log(error);
+      throw new Error("internal server error");
     }
+  }
 
-
-    static async findByUserId(userId) {
-
-        await RequestItem.exists({ userId: userId })
-            .catch((err) => {
-                throw new Error('user not found')
-            });
-
-        try {
-            return await RequestItem.find({userId: userId});
-        } catch (error) {
-            throw new Error('internal server error');
-        }
+  static async findById(requestId) {
+    try {
+      // const exists = await RequestItem.exists({ _id: requestId })
+      return await RequestItem.findById(requestId);
+    } catch (error) {
+      throw new Error("internal server error");
     }
+  }
 
-    static async findAll() {
-        try {
-            return  await RequestItem.find({});
-        } catch (error) {
-            throw new Error('Internal server error');
-        }
+  static async findByUserId(userId) {
+    await RequestItem.exists({ userId: userId }).catch((err) => {
+      throw new Error("user not found");
+    });
+
+    try {
+      return await RequestItem.find({ userId: userId });
+    } catch (error) {
+      throw new Error("internal server error");
     }
+  }
 
-
-    static async update(user, requestId, requestData) {
-        try {
-
-            return await RequestItem.findByIdAndUpdate(requestId, requestData, { new: true });
-        } catch (error) {
-            throw new Error('Internal server error');
-        }
+  static async findAll() {
+    try {
+      return await RequestItem.find({});
+    } catch (error) {
+      throw new Error("Internal server error");
     }
+  }
 
-    static async delete(requestId) {
-        try {
-            return await RequestItem.findByIdAndDelete(requestId );
-        } catch (error) {
-            throw new Error('Internal server error');
-        }
+  static async update(user, requestId, requestData) {
+    try {
+      return await RequestItem.findByIdAndUpdate(requestId, requestData, {
+        new: true,
+      });
+    } catch (error) {
+      throw new Error("Internal server error");
     }
+  }
 
-    static async notifyRequest(tag){
-
-        await RequestItem.exists({ tag: tag })
-            .catch((err) => {
-                throw new Error('Tag not found')
-            });
-
-        try {
-            const requestItems = await RequestItem.find({tag: tag});
-
-            let users = [];
-
-            for (let i = 0; i<requestItems.length; i++){
-                users.push(requestItems[i].userId)
-            }
-
-            const result = { users, tag };
-
-            publishRequestNotify(result)
-
-
-            return result
-
-
-        } catch (error) {
-            throw new Error('internal server error');
-        }
-
+  static async delete(requestId) {
+    try {
+      return await RequestItem.findByIdAndDelete(requestId);
+    } catch (error) {
+      throw new Error("Internal server error");
     }
+  }
 
+  static async notifyRequest(tag) {
+    await RequestItem.exists({ tag: tag }).catch((err) => {
+      throw new Error("Tag not found");
+    });
+
+    try {
+      const requestItems = await RequestItem.find({ tag: tag });
+
+      let users = [];
+
+      for (let i = 0; i < requestItems.length; i++) {
+        users.push(requestItems[i].userId);
+      }
+
+      const result = { users, tag };
+
+      publishRequestNotify(result);
+
+      return result;
+    } catch (error) {
+      throw new Error("internal server error");
+    }
+  }
 }
 
 module.exports = RequestItemService;
